141096346a16f84bc70b9ab355a22d6152c1d03c
Pichenze <34916327+Pichenze@users.noreply.github.com>
2019-08-06 20:45:39 +0800

    Create maxProfit.py

---
+++ maxProfit.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)==0:
+            return 0
+        lowprice = prices[0]
+        sumprofit = 0
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i] >= prices[i-1]:
+                profit = prices[i] - lowprice
+                if i == len(prices)-1:
+                    profit = prices[i]-lowprice
+                    sumprofit += profit
+            else:
+                profit = prices[i-1]-lowprice
+                lowprice = prices[i]
+                sumprofit += profit                
+        return sumprofit
