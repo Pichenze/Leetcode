873aa35befe621803c4180e7217e9954bca5799e
Pichenze <34916327+Pichenze@users.noreply.github.com>
2019-08-11 10:10:50 +0800

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -2,9 +2,18 @@ class Solution:
     def subsets(self, nums):
         n = len(nums)
         res = []
-        def trackback(i,tmp):  #回溯算法
+        def trackback(i,tmp):  #回溯算法，递归，dfs
             res.append(tmp)
             for j in range(i,n):
                 trackback(j+1,tmp+[nums[j]])
         trackback(0,[])
+        return res
+
+    
+class Solution:
+    def subsets(self, nums):
+        n = len(nums)
+        res = [[]]
+        for i in range(n): #迭代算法，闭包,bfs
+            res += [[nums[i]]+num for num in res]
         return res
