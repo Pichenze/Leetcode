5e67d05acbe138f4a051238776d10c50fdb8c523
Pichenze <34916327+Pichenze@users.noreply.github.com>
2019-08-11 10:04:35 +0800

    Create subsets.py

---
+++ subsets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsets(self, nums):
+        n = len(nums)
+        res = []
+        def trackback(i,tmp):  #回溯算法
+            res.append(tmp)
+            for j in range(i,n):
+                trackback(j+1,tmp+[nums[j]])
+        trackback(0,[])
+        return res
